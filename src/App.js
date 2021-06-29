8b39732b8843f197efd0035a280de3ce1cb9b3db
unknown <karimkgp@gmail.com>
2021-06-29 14:14:53 +0200

    limpio

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import './App.css';
+import React from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+    return (
+      <h1>Reactube App</h1>
+    );
+  }
+  
+  export default App;
